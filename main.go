273beecc1cb2865fc07f7020dfd6f8f4f07b1b02
Odery <kammurimini@gmail.com>
2023-06-21 18:36:48 +0300

    Some refactoring, stores protobuff data to a file for a test

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/Odery/gRPC-Playground/PBs"
+	"google.golang.org/protobuf/proto"
+	"log"
+	"os"
+)
+
+func main() {
+	p := &PBs.Person{
+		ID:       10,
+		Name:     "Ivan",
+		LastName: "Reznikov",
+		Age:      33,
+		Email:    "reznikov@mial.net",
+		Comment:  nil,
+	}
+
+	data, err := proto.Marshal(p)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = os.WriteFile("proto.data", data, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+}
