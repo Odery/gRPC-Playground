61bf7b8636ea3153e3d9dd157eb5f3321f2b0c48
Odery <kammurimini@gmail.com>
2023-06-28 13:14:16 +0300

    gRPC server is running and working as expected

--- main.go
+++ main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/Odery/gRPC-Playground/pb"
-	"google.golang.org/protobuf/proto"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
 	"log"
-	"os"
+	"net"
 )
 
+type CustomerServer struct {
+	pb.UnimplementedCustomerCreatorServer
+}
+
+func (_ CustomerServer) CreateCustomer(c context.Context, nc *pb.NewCustomer) (*pb.Customer, error) {
+	log.Printf("[INFO]: New Customer recieved: %v\n", nc)
+	if nc.Name != "" && nc.LastName != "" {
+		comment := "test Customer"
+		customer := &pb.Customer{
+			ID:        uuid.New().ID(),
+			Name:      nc.Name,
+			LastName:  nc.LastName,
+			Age:       nc.Age,
+			AutoLogin: "pseudo login link",
+			Comment:   &comment,
+		}
+		return customer, nil
+	} else {
+		return nil, fmt.Errorf("incorect Customer information")
+	}
+}
+
 func main() {
-	p := &pb.Person{
-		ID:       10,
-		Name:     "Ivan",
-		LastName: "Reznikov",
-		Age:      33,
-		Email:    "reznikov@mial.net",
-		Comment:  nil,
+	s, err := net.Listen("tcp", "0.0.0.0:8089")
+	if err != nil {
+		log.Fatalf("failed to start TCP listenere: %s", err)
 	}
 
-	data, err := proto.Marshal(p)
-	if err != nil {
-		log.Println(err)
-	}
+	server := grpc.NewServer()
+	service := new(CustomerServer)
 
-	err = os.WriteFile("proto.data", data, 0644)
+	pb.RegisterCustomerCreatorServer(server, service)
+
+	err = server.Serve(s)
 	if err != nil {
-		log.Println(err)
+		log.Fatal("failed to server Customer service")
 	}
 }
